ed2cce275c82db8ea2b62916c19f72c0ff2f0152
Mohammad Qumruzzaman <90256141+qumruzzaman@users.noreply.github.com>
2025-05-07 22:37:27 +0600

    Update project_4.php

--- project_4.php
+++ project_4.php
@@ -5,7 +5,7 @@
 //  If units are less than or equal to 200: bill = 50 * 3.50 + 50 * 4.00 + (units - 100) * 5.20
 //  If units are more than 200 means else case: bill = 50 * 3.50 + 50 * 4.00 + 100 * 5.20 + (units - 200) * 6.50
 // Show the total amount clearly with a message.
-$units = 100;
+$units = 101;
 $bill = 0;
 
 if($units<=50){
@@ -13,13 +13,10 @@ if($units<=50){
 }elseif($units<=100){
     $bill = (50 * 3.50) + ($units - 50) * 4.00;
 }elseif($units <=200){
-    $bill = (0 * 3.50) + (50 * 4.00) + ($units - 100) * 5.20;
-}else if($units > 200){
-    $bill = (50 * 3.50) + (50 * 4.00) + (100 * 5.20) + ($units - 200) * 6.50;
+    $bill = (50 * 3.50) + (50 * 4.00) + ($units - 100) * 5.20;
 }else{
-    $bill = "Invalid";
+    $bill = (50 * 3.50) + (50 * 4.00) + (100 * 5.20) + ($units - 200) * 6.50;
 }
-
 printf("Total bill for %d units is: %.2f Taka<br>", $units, $bill);
 
 ?>
